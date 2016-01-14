71d2d91a8d9e3095a3bf6e1e9da628766fe852d7
An1696ns <an1696ns@gmail.com>
2016-01-14 16:48:58 +0300

    init

---
+++ src/main/java/ru/spb/herzen/ivt3/App.java
@@ -0,0 +1,15 @@
+package ru.spb.herzen.ivt3;
+
+import javax.swing.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        JFrame test = new gui();
+    }
+}
---
+++ src/main/java/ru/spb/herzen/ivt3/gui.java
@@ -0,0 +1,17 @@
+package ru.spb.herzen.ivt3;
+
+import javax.swing.*;
+
+/**
+ * Created by Анна on 14.01.2016.
+ */
+public class gui extends JFrame {
+    private JPanel root;
+
+    public gui(){
+        super("TEST");
+        setContentPane(root);
+        pack();
+        setVisible(true);
+    }
+}
